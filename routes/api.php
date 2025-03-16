21d1fea694efe5a984c188c827d9c168573c633a
Bruno Correia <correiadesigner7@gmail.com>
2025-03-16 07:17:46 -0300

    api pronta

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 use App\Http\Controllers\TaskController;
 
 Route::get('/tasks', [TaskController::class, 'index']);
@@ -21,4 +9,3 @@ Route::post('/tasks', [TaskController::class, 'store']);
 Route::get('/tasks/{id}', [TaskController::class, 'show']);
 Route::put('/tasks/{id}', [TaskController::class, 'update']);
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-
